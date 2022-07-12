9dca1217cc74b1be5b8a0a2205340975f96c6d43
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2022-07-13 03:46:43 +0530

    Add register component in to App.js

--- register-form/src/App.js
+++ register-form/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
+import Register from "./register/Register";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <Register />
       </header>
     </div>
   );
